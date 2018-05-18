3d8e5d0fcc5ab1521cb06af914d4feeadc31ca2e
fernandopaniagua <fernando.paniagua@gmail.com>
2018-05-18 17:39:12 +0200

    Commit version inicial con armas

---
+++ Assets/GameAssets/_scripts/SoldierBehaviour.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierBehaviour : MonoBehaviour {
+    public float precision;
+    public int vida = 100;
+    private bool estaVivo = true;
+    public string mensajeVictoria = "¡He vencido!";
+    public char inicialEscuadron = 'U';
+    //Mele es "a hostia limpia"
+    public int danyoMele = 1;
+    public float tiempoEntreAtaques;
+
+    //OPCION 1, con GameObject
+    public GameObject prefabSangreGO;
+    //OPCION 2, con ParticleSystem
+    public ParticleSystem prefabSangrePS;
+
+
+    private Rigidbody cmpRigidbody;
+    private AudioClip dolor;
+    // Componentes: RigidBody, AudioSource, Collider .
+    // private Material materialPersonaje.
+    // private Texture / Texture2D / Texture3D.
+    public WeaponBehaviour weaponBehaviour;
+    public SoldierBehaviour enemigo;
+    	
+
+    // Use this for initialization.
+	void Start ()
+    {
+        precision = Random.value;
+        tiempoEntreAtaques = 0.1f;
+        cmpRigidbody = GetComponent<Rigidbody>();
+        ArrancarAtaquesRecurrentes();
+    }
+
+    // Update is called once per frame.
+    void Update () {
+        //weaponBehaviour.DispararA(enemigo);
+        if (!this.estaVivo)
+        {
+            cmpRigidbody.transform.Rotate(new Vector3(-0.01f, 0, 0));
+        }
+	}
+
+    private void AtacarCuerpoACuerpo() {
+        DecirMensaje("Voy a atacar a " + enemigo.name);
+        float probabilidadImpacto = Random.value;
+        bool heAcertado = probabilidadImpacto < this.precision;
+        if (enemigo.estaVivo && heAcertado)
+        {
+            this.enemigo.RecibirAtaque(danyoMele);
+            if (!enemigo.estaVivo)
+            {
+                DecirMensaje(this.mensajeVictoria);
+                CancelInvoke("AtacarCuerpoACuerpo");
+            }
+        }
+        else if (!heAcertado)
+        {
+            DecirMensaje("Arghh, he fallado");
+        }
+    }
+
+    public void RecibirAtaque(int danyoRecibido)
+    {
+        this.vida -= danyoRecibido;
+        DecirMensaje("Me han atacado y me queda " + this.vida + " de vida");
+        Sangrar();
+
+        if (this.vida <= 0)
+        {
+            Morir();
+        }
+    }
+
+    private void Sangrar()
+    {
+        //OPCION 1
+        GameObject nuevaSangreGO = Instantiate(prefabSangreGO);
+        nuevaSangreGO.transform.position = this.transform.position + Vector3.up;
+    }
+
+    private void Morir()
+    {
+        this.estaVivo = false;
+        DetenerAtaquesRecurrentes();
+        //this.gameObject.SetActive(false);
+        DecirMensaje("Arghh, he muerto");
+    }
+
+    private void ArrancarAtaquesRecurrentes()
+    {
+        InvokeRepeating("AtacarCuerpoACuerpo", tiempoEntreAtaques, tiempoEntreAtaques);
+    }
+
+    private void DetenerAtaquesRecurrentes()
+    {
+        this.CancelInvoke("AtacarCuerpoACuerpo");
+    }
+
+    public void DecirMensaje(string mensaje)
+    {
+        Debug.Log(this.name + " dice: " + mensaje);
+    }
+
+}
---
+++ Assets/GameAssets/_scripts/WeaponBehaviour.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBehaviour : MonoBehaviour {
+    public AudioClip audioRecarga;
+    public AudioClip audioDisparo;
+
+    public int capacidadCargador = 10;
+    private int balasEnCargador;
+
+    public float balasPorSegundo = 1.0f;
+    public float precision = 0.9f;
+
+    public int danyo = 1;
+
+    bool estoyRecargando = false;
+    public float tiempoRecarga = 0.5f;
+
+    public SoldierBehaviour soldadoPortador;
+
+    void Start () {
+        this.balasEnCargador = this.capacidadCargador;
+	}
+
+    public void DispararA(SoldierBehaviour enemigo)
+    {
+        if(balasEnCargador>0 && !estoyRecargando)
+        {
+            soldadoPortador.DecirMensaje("Disparando a " + enemigo.name + " con " + this.name);
+            soldadoPortador.DecirMensaje("Me quedan " + balasEnCargador + " balas");
+            balasEnCargador -= 1;
+            bool disparoAcierta = Random.value < precision;
+            if (disparoAcierta)
+            {
+                enemigo.RecibirAtaque(danyo);
+            }
+            else
+            {
+                soldadoPortador.DecirMensaje("Jarl, he fallado");
+            }
+        }
+        else if (balasEnCargador == 0 && !estoyRecargando)
+        {
+            Recargar();
+        }
+    }
+
+    public void Recargar()
+    {
+        soldadoPortador.DecirMensaje("Iniciando recarga de " + this.name);
+        estoyRecargando = true;
+        AudioSource.PlayClipAtPoint(audioRecarga, this.transform.position);
+        Invoke("FinalizarRecarga", tiempoRecarga);
+    }
+    private void FinalizarRecarga()
+    {
+        soldadoPortador.DecirMensaje("Finalizando recarga " + this.name);
+        balasEnCargador = capacidadCargador;
+        estoyRecargando = false;
+    }
+   
+}
